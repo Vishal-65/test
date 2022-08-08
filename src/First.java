b6283046f40b4e16a9bb6ee9ae619228db328377
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-08 15:23:47 +0530

    Second Commit

--- src/First.java
+++ src/First.java
@@ -1,2 +1,26 @@
+import java.util.*;
 public class First {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int n=sc.nextInt();
+        int m1=x-n;
+        int m2=y-n;
+        int m3=z-x;
+
+        String res="";
+        if((m1<m2 && m1>=0) && (m1<m3 && m1>=0) ){
+            res="L1";
+        }
+        else if((m2<m1 && m2>=0) && (m2<m3 && m2>=0) ){
+            res="L2";
+        }
+        else if((m3<m1 && m3>=0) && (m3<m2 && m3>=0) ){
+            res="L3";
+        }
+        System.out.print(res);
+
+    }
 }
