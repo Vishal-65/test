639bedadcb3dfb495b4fe4ea11d9831cc3a57bf1
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-09 18:54:37 +0530

    FINAL COMMIT

--- src/Day2/HolidayMain.java
+++ src/Day2/HolidayMain.java
@@ -1,3 +1,20 @@
+/*The Java class called Holiday is started below. An object of class Holiday represents a holiday during the year. This class has three instance
+ variables:
+  name, which is a String representing the name of the holiday
+  day, which is an int representing the day of the month of the holiday
+  month, which is a String representing the month the holiday is in
+  Write a constructor for the class Holiday, which takes a String representing the name, an int representing the day, and a String representing
+  the month as its arguments, and sets the class variables to these values.
+
+  Write a method inSameMonth, which compares two instances of the class Holiday, and returns the Boolean value true if they have the same month,
+   and false if they do not.
+
+  Write a method avgDate which takes an array of base type Holiday as its argument, and returns a double that is the average of the day variables
+   in the Holiday instances in the array. You may assume that the array is full (i.e. does not have any null entries).
+
+  Write a piece of code that creates a Holiday instance with the name “Independence Day”, with the day “4”, and with the month “July”. */
+
+
 package Day2;
 class Holiday{
     private String name;
@@ -8,24 +25,30 @@ class Holiday{
         this.day=day;
         this.month=month;
     }
-    public boolean inSameMonth(Holiday Object1){
-        return this.month.equals(Object1.month);
+    public static boolean inSameMonth(Holiday holiday1,Holiday holiday2){
+        return holiday1.month.equals(holiday2.month);
     }
-    public static double avgDate(Holiday[] Object1){
+    public static double avgDate(Holiday[] holiday_array){
         int res=0;
-        for(int i=0;i<Object1.length;i++){
-            res+=Object1[i].day;
+        for(int i=0;i<holiday_array.length;i++){
+            res+=holiday_array[i].day;
         }
-        return ((double)res)/Object1.length;
+        return ((double)res)/holiday_array.length;
     }
 
 
 }
 public class HolidayMain {
     public static void main(String[] args) {
-        Holiday[] Object1 =new Holiday[2];
-        Object1[0]=new Holiday("Independance Day",4,"July");
-        Object1[1]=new Holiday("Republic Day",4,"January");
+
+      Holiday holiday1=new Holiday("Independance Day",4,"July");
+      Holiday holiday2=new Holiday("Republic Day",4,"July");
+      System.out.println(Holiday.inSameMonth(holiday1,holiday2));
+      Holiday [] holiday_array=new Holiday[2];
+      holiday_array[0]=holiday1;
+      holiday_array[1]=holiday2;
+      System.out.println(Holiday.avgDate(holiday_array));
+
 
     }
 }
