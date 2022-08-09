816973ea9722522d557aa9074282701664ce743e
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-09 09:58:50 +0530

    Final Commit

--- src/First.java
+++ src/First.java
@@ -2,25 +2,29 @@ import java.util.*;
 public class First {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int a[]=new int[3];
+        for(int i=0;i<3;i++){
+          a[i]=sc.nextInt();
+        }
         int x=sc.nextInt();
-        int y=sc.nextInt();
-        int z=sc.nextInt();
-        int n=sc.nextInt();
-        int m1=x-n;
-        int m2=y-n;
-        int m3=z-x;
+        int maxi=Integer.MAX_VALUE;
+        for(int i=0;i<3;i++){
+          int smallest=a[i]-x;
+          if(maxi>smallest && smallest>0){
+              maxi=smallest;
 
-        String res="";
-        if((m1<m2 && m1>=0) && (m1<m3 && m1>=0) ){
-            res="L1";
+          }
         }
-        else if((m2<m1 && m2>=0) && (m2<m3 && m2>=0) ){
-            res="L2";
+
+        if(x+maxi==a[0]){
+            System.out.print("L1");
         }
-        else if((m3<m1 && m3>=0) && (m3<m2 && m3>=0) ){
-            res="L3";
+        else if(x+maxi==a[1]){
+            System.out.print("L2");
         }
-        System.out.print(res);
+        else{
+            System.out.print("L3");
+        }
 
     }
 }
