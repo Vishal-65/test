9a81f2a406a07d3ad0d2b4aed62d5c7bd572c3db
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-09 13:03:18 +0530

    [ADD]MEAN

--- src/Day2/MeanMedianMode.java
+++ src/Day2/MeanMedianMode.java
@@ -1,4 +1,24 @@
 package Day2;
-
+import java.util.*;
 public class MeanMedianMode {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int Mean=FindMean(arr);
+        int Median=FindMedian(arr);
+        int Mode=FindMode(arr);
+
+    }
+    public static int FindMean(int arr[]){
+        int array_length=arr.length;
+        int sum_elements=0;
+        for(int i=0;i<arr.length;i++){
+            sum_elements=sum_elements+arr[i];
+        }
+        return sum_elements/array_length;
+    }
 }
