61c6999dfb84ded442e812e0f9f098a21416f92e
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-10 22:45:07 +0530

    Created the ElectionException Class

--- src/Day3/Election.java
+++ src/Day3/Election.java
@@ -1,5 +1,11 @@
 package Day3;
+import java.util.*;
+class ElectionException extends Exception{
+    public ElectionException(String message){
+        super(message);
+    }
+}
 
 public class Election {
-    
+
 }
