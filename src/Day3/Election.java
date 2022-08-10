a5ab2860835929f3b830227ddd9defb144477c90
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-10 22:51:20 +0530

    Implemented The Try /catch blocks in Main Function.

--- src/Day3/Election.java
+++ src/Day3/Election.java
@@ -7,5 +7,18 @@ class ElectionException extends Exception{
 }
 
 public class Election {
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Nominee Age = ");
+        int age=sc.nextInt();
+        try{
+            if(age<25){
+                throw new ElectionException("Nominee's Age is Less than 25,Cannot participate in Nomination.");
+            }
+            System.out.println("You are the Nominee, You Can participate in Nomination");
+        }
+        catch(Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
 }
