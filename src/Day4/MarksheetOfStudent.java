fe2503ef4c461891c0a214785b02366081f017d9
Vishal-65 <Vishal.Vishwanath@ivycomptech.com>
2022-08-11 12:44:02 +0530

    Removed the Subject -02 and calculated the Total and percentage

--- src/Day4/MarksheetOfStudent.java
+++ src/Day4/MarksheetOfStudent.java
@@ -11,15 +11,19 @@ public class MarksheetOfStudent {
            Marksheet.add(sc.nextInt());
            total=total+Marksheet.get(position);
        }
-
+       System.out.println(Marksheet.size());
        System.out.println("Marks Of All Subjects:"+Marksheet);
        System.out.println("Total Marks:"+total);
        double percentage=(double)(total/(Marksheet.size()*100)*100);
-       System.out.print("Percentage: "+percentage);
-       
-        // removed the marks of subject-02
+       System.out.println("Percentage: "+percentage);
 
+        // removed the marks of subject-02
+         total=0;
         Marksheet.remove(1);
+        for(int position=0;position<Marksheet.size();position++){
+            total=total+Marksheet.get(position);
+        }
+        System.out.println(Marksheet.size());
         System.out.println("Marks Of All Subjects:"+Marksheet);
         System.out.println("Total Marks:"+total);
         percentage=(double)(total/(Marksheet.size()*100)*100);
